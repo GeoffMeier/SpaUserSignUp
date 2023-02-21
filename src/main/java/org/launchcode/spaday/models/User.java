72473b56ad8980988bf85df278dd2b26e97b241c
Geoffrey Meier <grm0809@gmail.com>
2023-02-20 20:33:29 -0600

    Studio Spa-day

---
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -0,0 +1,39 @@
+package org.launchcode.spaday.models;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private static int nextId=1;
+    private String userName;
+    private String email;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
